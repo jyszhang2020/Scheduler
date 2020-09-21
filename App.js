5858d16c5d5b949c067a9d0c274a42d69c14d83e
Joyce Feifei <jyszhang2020@gmail.com>
2020-09-21 00:26:56 -0500

    Filtering

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import CourseList from './components/CourseList';
 import Course from './components/Course';
 import Banner from './components/Banner';
 
 const App = () => {
+
   const [schedule, setSchedule] = useState ({ title: '', courses: [] });
 
   const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
--- components/CourseList.js
+++ components/CourseList.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Course from './Course';
+import TermSelector from './TermSelector';
 
-const CourseList = ({ courses }) => (
-    <ScrollView>
-      <View style={styles.courseList}>
-        { courses.map(course => <Course key={course.id} course={ course } />) }
-      </View>
-    </ScrollView>
-  );
+const termMap = { F: 'Fall', W: 'Winter', S: 'Spring'};
+const terms = Object.values(termMap);
+
+const getCourseTerm = course => (
+  termMap[course.id.charAt(0)]
+);
+
+const CourseList = ({courses}) => {
+    // set the initial to Fall when you open page
+    const [selectedTerm, setSelectedTerm] = useState('Fall');
+    const termCourses = courses.filter(course => selectedTerm === getCourseTerm(course));
+    
+    return (
+      <ScrollView>
+        <TermSelector selectedTerm={selectedTerm} 
+                      setSelectedTerm = {setSelectedTerm}
+        />
+        { termCourses.map(course => <Course key={course.id} course={course} />) }
+      </ScrollView>
+    );
+};
 
 const styles = StyleSheet.create({
     courseList: {
