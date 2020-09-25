2b0c0aa1e4d7ad83f0d99fe0e22538b4d7fe0ef3
Joyce Feifei <jyszhang2020@gmail.com>
2020-09-24 21:20:50 -0500

    Database fetch

--- screens/ScheduleScreen.js
+++ screens/ScheduleScreen.js
@@ -3,27 +3,30 @@ import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } fr
 import CourseList from '../components/CourseList';
 import Banner from '../components/Banner';
 import UserContext from '../UserContext';
+import { firebase } from '../firebase';
+
+const fixCourses = json => ({
+    ...json,
+    courses: Object.values(json.courses)
+});
 
 const ScheduleScreen = ({navigation}) => {
     const user = useContext(UserContext);
     const canEdit = user && user.role === 'admin';
     const [schedule, setSchedule] = useState({ title: '', courses: [] });
 
-    const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
-
     const view = (course) => {
         navigation.navigate(canEdit ? 'CourseEditScreen' : 'CourseDetailScreen', { course });
     };
 
-    useEffect( () => {
-    const fetchSchedule = async () => {
-        const response = await fetch(url);
-        if (!response.ok) throw response;
-        const json = await response.json();
-        setSchedule(json);
-    }
-    fetchSchedule();
-    }, []);
+    useEffect(() => {
+        const db = firebase.database().ref();
+        const handleData = snap => {
+          if (snap.val()) setSchedule(fixCourses(snap.val()));
+        }
+        db.on('value', handleData, error => alert(error));
+        return () => { db.off('value', handleData); };
+      }, []);
 
     return (
     <SafeAreaView style={styles.container}>
